ed2ab72e3a85208b4f8d886ddbd128ad26667822
Valesh Gopal <valesh.dev@gmail.com>
2022-05-31 15:49:18 +0530

    Improve Edit Functionality

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import ListItems from './components/ListItems';
 import { v4 as uuid } from 'uuid';
 import './App.css';
 
+const getLocalStorage = () => {
+  const localStorageItems = localStorage.getItem('items');
+  return localStorageItems ? JSON.parse(localStorageItems) : [];
+};
+
 const App = () => {
   const [task, setTask] = useState('');
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
 
   const inputRef = useRef();
 
@@ -18,9 +26,20 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (task !== '') {
+    if (!task) return;
+    else if (task && isEditing) {
+      setItems((prevItems) =>
+        prevItems.map((item) =>
+          item.id === editId ? { ...item, taskName: task } : item
+        )
+      );
+      setTask('');
+      setEditId(null);
+      setIsEditing(false);
+    } else {
       setItems([...items, { id, taskName: task }]);
       setTask('');
+      setIsEditing(false);
     }
 
     inputRef.current.value = '';
@@ -28,13 +47,12 @@ const App = () => {
   };
 
   const handleEdit = (id) => {
-    // setEditItem(true);
+    setIsEditing(true);
     inputRef.current.focus();
     const newTask = items.find((item) => item.id === id).taskName;
     setTask(newTask);
+    setEditId(id);
     inputRef.current.value = newTask;
-    const newItems = items.filter((item) => item.id !== id);
-    setItems([...newItems]);
   };
 
   const handleDelete = (id) => {
@@ -49,18 +67,23 @@ const App = () => {
     inputRef.current.focus();
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
   return (
     <main className='container'>
+      <h2 className='heading'>To Do App</h2>
       <form className='todo-form'>
         <input
           className='input-field'
           ref={inputRef}
           type='text'
-          placeholder='Enter a new item'
+          placeholder='Enter a new task'
           onChange={handleChange}
         />
         <button className='btn' onClick={handleSubmit}>
-          Add Task
+          {isEditing ? 'Update' : 'Add Task'}
         </button>
       </form>
 
